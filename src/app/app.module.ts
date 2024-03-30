fdb429a3b3bdaa906c0b318a7e4bd46681565702
Martin Lopez <mlopez@vantek.co>
2024-03-30 19:21:14 -0300

    Primer Commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,17 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { BodyComponent } from './components/body/body.component';
+import { FooterComponent } from './components/footer/footer.component';
+//Aca le digo a angular que componentes y servicios tiene
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    BodyComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/components/header/header.component.ts
@@ -0,0 +1,12 @@
+import { Component } from "@angular/core";
+
+@Component({
+    selector: 'app-header',                    //Defino como angular va a cargar el componente 
+    templateUrl: './header.component.html' 
+})
+
+//Para que lo pueda usar debo de exportar el componente
+
+export class HeaderComponent {
+    
+}
